21a0a48fbe79b3534b2b187b001ad55ab98068ca
Sanya Onlyfans <sasha.fedorenko4736@gmail.com>
2026-02-01 16:54:41 +0300

    дописал под модели на discord фронте

--- app/Http/Resources/Api/Discord/EventFullResource.php
+++ app/Http/Resources/Api/Discord/EventFullResource.php
@@ -10,8 +10,8 @@ class EventFullResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'name' => $this->name, // ДОБАВЛЕНО
-            'description' => $this->description, // ДОБАВЛЕНО
+            'name' => $this->name,
+            'description' => $this->description,
             'region' => $this->region,
             'start_at' => $this->start_at->toIso8601String(),
             'total_slots' => $this->total_slots,
@@ -20,7 +20,11 @@ class EventFullResource extends JsonResource
             'discord_message_id' => $this->discord_message_id,
             'public_channel_id' => $this->guild->public_channel_id ?? null,
             'squads' => SquadResource::collection($this->squads),
-            'reserve' => ParticipantResource::collection($this->participants->whereNull('squad_id')),
+            'reserve' => ParticipantResource::collection(
+                $this->participants
+                    ->whereNull('squad_id')
+                    ->where('status', 'confirmed')
+            ),
             'stats' => [
                 'total_confirmed' => $this->participants->where('status', 'confirmed')->count(),
                 'total_filled' => $this->participants->count(),
--- app/Http/Resources/Api/Discord/ParticipantResource.php
+++ app/Http/Resources/Api/Discord/ParticipantResource.php
@@ -9,9 +9,12 @@ class ParticipantResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'discord_id' => $this->user->discord_id,
+
             'display_name' => $this->user->profile->family_name
                 ?? $this->user->global_name
                     ?? $this->user->username,
+
             'status' => $this->status,
         ];
     }
