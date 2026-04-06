aa5e57451a274408b9be0e6778b2a362635dd2bd
pamallika <sasha.fedorenko4736@gmail.com>
2026-04-06 16:55:36 +0300

    fix accept

--- app/Http/Resources/Api/V1/GuildMemberResource.php
+++ app/Http/Resources/Api/V1/GuildMemberResource.php
@@ -16,6 +16,7 @@ class GuildMemberResource extends JsonResource
     {
         return [
             'id' => $this->id,
+            'user_id' => $this->user_id,
             'guild' => new GuildResource($this->whenLoaded('guild')),
             'user' => new UserResource($this->whenLoaded('user')),
             'role' => $this->role,
